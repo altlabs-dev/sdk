7abe36c70821be8f2fec113ca1c98d27de7b96ed
Uniswap <153300677+ffierce@users.noreply.github.com>
2024-06-06 11:43:44 +0700

    up

--- src/constants.ts
+++ src/constants.ts
@@ -4,6 +4,6 @@ export type BigintIsh = JSBI | string | number
 
 export type Address = `0x${ string }`
 
-export const MAX_UINT_256 = JSBI.BigInt('0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff')
+export const MAX_UINT_256: BigintIsh = JSBI.BigInt('0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff')
 
-export const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000'
+export const ZERO_ADDRESS: Address  = '0x0000000000000000000000000000000000000000'
